10d207b801bcbdd2189192ee2cfd056ea815cc58
Clémence <clemvoegele@gmail.com>
2021-05-10 10:35:08 +0200

    Import IA + Reload

--- p5Template-master/build/build.js
+++ p5Template-master/build/build.js
@@ -1,16 +1,43 @@
+var dance;
+var z = [];
 var gui = new dat.GUI();
 var params = {
-    Ellipse_Size: 30,
     Download_Image: function () { return save(); },
 };
-gui.add(params, "Ellipse_Size", 0, 100, 1);
 gui.add(params, "Download_Image");
+var modele = new rw.HostedModel({
+    url: "https://fashion-illustrations-d515a0ce.hosted-models.runwayml.cloud/v1/",
+    token: "clAH7CRoH1FdrNmmZUY1DA==",
+});
 function draw() {
     background(0);
-    ellipse(mouseX, mouseY, params.Ellipse_Size);
+    if (dance) {
+        image(dance, 0, 0, width, height);
+    }
+}
+function generateDance() {
+    var data = {
+        z: z,
+        truncation: 0.8
+    };
+    modele.info().then(function (info) { return console.log(info); });
+    modele.query(data).then(gotImage);
+}
+function gotError(error) {
+    console.error(error);
+}
+function gotImage(result) {
+    dance = createImg(result.image);
+    dance.hide();
+    z[0] += 0.1;
+    generateDance();
 }
 function setup() {
     p6_CreateCanvas();
+    for (var i = 0; i < 512; i++) {
+        z[i] = random(-0.1, 0.1);
+    }
+    createButton('dance').mousePressed(generateDance);
 }
 function windowResized() {
     p6_ResizeCanvas();
--- p5Template-master/src/sketch.ts
+++ p5Template-master/src/sketch.ts
@@ -2,13 +2,21 @@
 //  Parameters and UI
 // -------------------
 
+let dance;
+
+const z = [];
+
 const gui = new dat.GUI()
 const params = {
-    Ellipse_Size: 30,
     Download_Image: () => save(),
 }
-gui.add(params, "Ellipse_Size", 0, 100, 1)
 gui.add(params, "Download_Image")
+//@ts-ignore
+const modele = new rw.HostedModel({
+    url: "https://fashion-illustrations-d515a0ce.hosted-models.runwayml.cloud/v1/",
+    token: "clAH7CRoH1FdrNmmZUY1DA==",
+  });
+  
 
 // -------------------
 //       Drawing
@@ -16,8 +24,36 @@ gui.add(params, "Download_Image")
 
 function draw() {
     background(0)
-    ellipse(mouseX, mouseY, params.Ellipse_Size)
+    if (dance) 
+    {
+        image(dance, 0, 0,width,height);
+    }
 }
+
+function generateDance() {
+    // httpPost(path, [datatype], [data], [callback], [errorCallback])
+
+    const data = {
+      z: z,
+      truncation: 0.8
+    };
+
+    //// You can use the info() method to see what type of input object the model expects
+    modele.info().then(info => console.log(info));
+
+    modele.query(data).then(gotImage);
+  }
+
+  function gotError(error) {
+    console.error(error);
+  }
+  
+  function gotImage(result) {
+    dance = createImg(result.image);
+    dance.hide();
+    z[0]+=0.1;
+    generateDance();
+  }
 
 // -------------------
 //    Initialization
@@ -25,6 +61,10 @@ function draw() {
 
 function setup() {
     p6_CreateCanvas()
+    for (let i = 0; i < 512; i++) {
+        z[i] = random(-0.1, 0.1);
+    }
+    createButton('dance').mousePressed(generateDance);
 }
 
 function windowResized() {
